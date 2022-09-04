0af27cf83366e3a079838c9c141bc061e74012c5
jash.liao <jash.liao@gmail.com>
2022-09-04 08:53:29 +0800

    Add 202209040853

---
+++ 123.bat
@@ -0,0 +1,2 @@
+ffmpeg -ss 00:18:00 -t 00:06:00 -i "videoplayback.mp4" -vcodec copy -acodec copy  "01.mp4"
+ffmpeg -ss 00:18:00 -t 00:15:02 -i "videoplayback.mp4" -vcodec copy -acodec copy  "02.mp4"
